edfd217bec3af12ef30b9fb4d6c8ed256a1ff62d
Javier Flores <javi.2903@outlook.es>
2024-05-22 20:04:01 -0500

    pdf 1 caso practico 1

---
+++ SeguimientoAlgoritmos.js
@@ -0,0 +1,20 @@
+/* 
+Algoritmo 1
+Indica  el  valor  de  las  variables  A  y  B  al  finalizar  el  siguiente  
+algoritmo: */
+let A = 7;
+let B = 4;
+let C = 9;
+while (A != C) {
+  if (C > 9) {
+    B = B + 5;
+  } else {
+    A = A + 2;
+  }
+  console.log("A: ", A, "B: ", B);
+}
+
+/* 
+Algoritmo 2
+Indica  el  valor  de  las  variables  A  y  B  al  finalizar  el  siguiente  
+algoritmo: */
