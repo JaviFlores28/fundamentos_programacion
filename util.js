0a020d2a74bc657daeb2451e6bdc87150664d21c
Javier Flores <javi.2903@outlook.es>
2024-06-07 15:30:18 -0500

    metodo de la burbuja

--- util.js
+++ util.js
@@ -52,6 +52,19 @@ arrayTextos.sort((a, b) => {
 console.log(arrayTextos);
   
   */
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
 //ejemplo metodo burbuja
 function bubbleSort(array) {
   let n = array.length;
