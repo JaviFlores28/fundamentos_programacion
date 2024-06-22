364de21c734e78c50fe826e5022fa754f9dfc5da
Javier Flores <javi.2903@outlook.es>
2024-06-22 18:12:22 -0500

    Update util.js

--- util.js
+++ util.js
@@ -1,6 +1,4 @@
 //cambio1
-//cambio guthub
-//cambio texto
 // archivo util para el algoritmo 1 de ordenacion de textos de arrays
 /* reformar mediante metodo map
 let textostamaño = textos.map((texto) => [
